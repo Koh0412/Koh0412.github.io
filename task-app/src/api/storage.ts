fcc8a535013528ac9a155e7ad0c34ff89720a3b9
kohei.hujita <point.out92@gmail.com>
2020-07-13 22:22:23 +0900

    fix tasks not clear

--- task-app/src/api/storage.ts
+++ task-app/src/api/storage.ts
@@ -12,6 +12,7 @@ export class Storage {
     if (confirm) {
       localStorage.clear();
       idAttr.taskCount.textContent = localStorage.length.toLocaleString();
+      idAttr.tasks.innerHTML = "";
     }
   }
 
